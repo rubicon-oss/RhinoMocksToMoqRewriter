897dc0114eb9908bdd87e2b2f85575838b845b55
Jakob Müllner <jakob.muellner@rubicon.eu>
2021-05-12 14:37:58 +0200

    Update rewriter names

---
+++ Core/Rewriters/ObsoleteMethodRewriter.cs
@@ -0,0 +1,108 @@
+﻿//  Copyright (c) rubicon IT GmbH
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace RhinoMocksToMoqRewriter.Core.Rewriters
+{
+  public class ObsoleteMethodRewriter : RewriterBase
+  {
+    public override SyntaxNode? VisitMethodDeclaration (MethodDeclarationSyntax node)
+    {
+      var rhinoMocksExtensionsCompilationSymbol = Model.Compilation.GetTypeByMetadataName ("Rhino.Mocks.RhinoMocksExtensions");
+      var rhinoMocksMockRepositoryCompilationSymbol = Model.Compilation.GetTypeByMetadataName ("Rhino.Mocks.MockRepository");
+      if (rhinoMocksExtensionsCompilationSymbol == null || rhinoMocksMockRepositoryCompilationSymbol == null)
+      {
+        throw new InvalidOperationException ("Rhino.Mocks cannot be found.");
+      }
+
+      var nodesToBeReplaced = GetNodesToBeReplaced (node, rhinoMocksMockRepositoryCompilationSymbol, rhinoMocksExtensionsCompilationSymbol);
+      return node.RemoveNodes (nodesToBeReplaced, SyntaxRemoveOptions.KeepNoTrivia);
+    }
+
+    public override SyntaxNode? VisitFieldDeclaration (FieldDeclarationSyntax node)
+    {
+      var rhinoMocksMockRepositoryCompilationSymbol = Model.Compilation.GetTypeByMetadataName ("Rhino.Mocks.MockRepository");
+      if (rhinoMocksMockRepositoryCompilationSymbol == null)
+      {
+        throw new InvalidOperationException ("Rhino.Mocks cannot be found.");
+      }
+
+      var mockRepositoryTypeSymbol = ModelExtensions.GetSymbolInfo (Model, node.Declaration.Type).Symbol;
+      return rhinoMocksMockRepositoryCompilationSymbol.Equals (mockRepositoryTypeSymbol, SymbolEqualityComparer.Default)
+          ? null
+          : node;
+    }
+
+    private IEnumerable<SyntaxNode> GetNodesToBeReplaced (
+        MethodDeclarationSyntax node,
+        INamedTypeSymbol rhinoMocksMockRepositoryCompilationSymbol,
+        INamedTypeSymbol rhinoMocksExtensionsCompilationSymbol)
+    {
+      return GetObsoleteExpressionStatements (node, GetSimpleMockSymbols (rhinoMocksExtensionsCompilationSymbol, rhinoMocksMockRepositoryCompilationSymbol))
+          .Concat (GetObsoleteLocalDeclarationStatements (node, rhinoMocksMockRepositoryCompilationSymbol))
+          .Concat (GetObsoleteAssignmentExpressions (node, rhinoMocksMockRepositoryCompilationSymbol));
+    }
+
+    private IEnumerable<ExpressionStatementSyntax> GetObsoleteAssignmentExpressions (
+        MethodDeclarationSyntax node,
+        INamedTypeSymbol rhinoMocksMockRepositoryCompilationSymbol)
+    {
+      return node.DescendantNodes()
+          .Where (s => s.IsKind (SyntaxKind.ExpressionStatement))
+          .Select (s => (ExpressionStatementSyntax) s)
+          .Where (s => s.Expression.IsKind (SyntaxKind.SimpleAssignmentExpression))
+          .Where (
+              s => s.Expression is AssignmentExpressionSyntax assignmentExpression
+                   && rhinoMocksMockRepositoryCompilationSymbol.Equals (Model.GetTypeInfo (assignmentExpression).Type, SymbolEqualityComparer.Default));
+    }
+
+    private IEnumerable<SyntaxNode> GetObsoleteLocalDeclarationStatements (
+        MethodDeclarationSyntax node,
+        INamedTypeSymbol rhinoMocksMockRepositoryCompilationSymbol)
+    {
+      return node.DescendantNodes()
+          .Where (s => s.IsKind (SyntaxKind.LocalDeclarationStatement))
+          .Select (s => (LocalDeclarationStatementSyntax) s)
+          .Where (s => rhinoMocksMockRepositoryCompilationSymbol.Equals (Model.GetSymbolInfo (s.Declaration.Type).Symbol, SymbolEqualityComparer.Default));
+    }
+
+    private IEnumerable<SyntaxNode> GetObsoleteExpressionStatements (
+        MethodDeclarationSyntax node,
+        IEnumerable<ISymbol> simpleMockSymbols)
+    {
+      return node.DescendantNodes()
+          .Where (s => s.IsKind (SyntaxKind.ExpressionStatement))
+          .Select (s => (ExpressionStatementSyntax) s)
+          .Where (
+              s => s.Expression is InvocationExpressionSyntax invocationExpression
+                   && Model.GetSymbolInfo (invocationExpression).Symbol is IMethodSymbol methodSymbol
+                   && simpleMockSymbols.Contains (methodSymbol.ReducedFrom ?? methodSymbol.OriginalDefinition, SymbolEqualityComparer.Default));
+    }
+
+    private static IEnumerable<ISymbol> GetSimpleMockSymbols (
+        INamedTypeSymbol rhinoMocksExtensionsCompilationSymbol,
+        INamedTypeSymbol rhinoMocksMockRepositoryCompilationSymbol)
+    {
+      return rhinoMocksExtensionsCompilationSymbol.GetMembers ("Replay")
+          .Concat (rhinoMocksMockRepositoryCompilationSymbol.GetMembers ("ReplayAll"))
+          .Concat (rhinoMocksExtensionsCompilationSymbol.GetMembers ("BackToRecord"))
+          .Concat (rhinoMocksMockRepositoryCompilationSymbol.GetMembers ("BackToRecordAll"));
+    }
+  }
+}
---
+++ Core/Rewriters/Strategies/ConstraintStrategies/ConstraintRewriteStrategyFactory.cs
@@ -0,0 +1,91 @@
+﻿//  Copyright (c) rubicon IT GmbH
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+namespace RhinoMocksToMoqRewriter.Core.Rewriters.Strategies.ConstraintStrategies
+{
+  public static class ConstraintRewriteStrategyFactory
+  {
+    public static IConstraintRewriteStrategy GetRewriteStrategy (ExpressionSyntax node, SemanticModel model)
+    {
+      var symbol = model.GetSymbolInfo (node).Symbol;
+      if (symbol == null)
+      {
+        return DefaultConstraintRewriteStrategy.Instance;
+      }
+
+      var rhinoMocksConstraintsIsSymbol = model.Compilation.GetTypeByMetadataName ("Rhino.Mocks.Constraints.Is");
+      var rhinoMocksConstraintsListSymbol = model.Compilation.GetTypeByMetadataName ("Rhino.Mocks.Constraints.List");
+      var rhinoMocksConstraintsPropertySymbol = model.Compilation.GetTypeByMetadataName ("Rhino.Mocks.Constraints.Property");
+      if (rhinoMocksConstraintsIsSymbol == null || rhinoMocksConstraintsListSymbol == null || rhinoMocksConstraintsPropertySymbol == null)
+      {
+        throw new InvalidOperationException ("Rhino.Mocks cannot be found!");
+      }
+
+      if (IsNotAutomaticallyRewritable (node, symbol, model, rhinoMocksConstraintsIsSymbol, rhinoMocksConstraintsPropertySymbol))
+      {
+        Console.Error.WriteLine (
+            $"  WARNING: Unable to convert Rhino.Mocks.Constraints.Property.Value or Rhino.Mocks.Constraints.Is.NotNull\r\n"
+            + $"  {node.SyntaxTree.FilePath} at line {node.GetLocation().GetMappedLineSpan().StartLinePosition.Line}");
+
+        return DefaultConstraintRewriteStrategy.Instance;
+      }
+
+      return symbol switch
+      {
+          _ when rhinoMocksConstraintsIsSymbol.GetMembers ("Equal").Contains (symbol) => IsEqualConstraintRewriteStrategy.Instance,
+          _ when rhinoMocksConstraintsIsSymbol.GetMembers ("NotEqual").Contains (symbol) => IsNotEqualConstraintRewriteStrategy.Instance,
+          _ when rhinoMocksConstraintsIsSymbol.GetMembers ("Same").Contains (symbol) => IsSameConstraintRewriteStrategy.Instance,
+          _ when rhinoMocksConstraintsIsSymbol.GetMembers ("NotSame").Contains (symbol) => IsNotSameConstraintRewriteStrategy.Instance,
+          _ when rhinoMocksConstraintsIsSymbol.GetMembers ("Null").Contains (symbol) => IsNullConstraintRewriteStrategy.Instance,
+          _ when rhinoMocksConstraintsIsSymbol.GetMembers ("NotNull").Contains (symbol) => IsNotNullConstraintRewriteStrategy.Instance,
+          _ when rhinoMocksConstraintsIsSymbol.GetMembers ("GreaterThan").Contains (symbol) => IsGreaterThanConstraintRewriteStrategy.Instance,
+          _ when rhinoMocksConstraintsIsSymbol.GetMembers ("GreaterThanOrEqual").Contains (symbol) => IsGreaterThanOrEqualConstraintRewriteStrategy.Instance,
+          _ when rhinoMocksConstraintsIsSymbol.GetMembers ("LessThan").Contains (symbol) => IsLessThanConstraintRewriteStrategy.Instance,
+          _ when rhinoMocksConstraintsIsSymbol.GetMembers ("LessThanOrEqual").Contains (symbol) => IsLessThanOrEqualConstraintRewriteStrategy.Instance,
+          _ when rhinoMocksConstraintsListSymbol.GetMembers ("IsIn").Contains (symbol) => ListIsInConstraintRewriteStrategy.Instance,
+          _ when rhinoMocksConstraintsListSymbol.GetMembers ("ContainsAll").Contains (symbol) => ListContainsAllConstraintRewriteStrategy.Instance,
+          _ when rhinoMocksConstraintsPropertySymbol.GetMembers ("Value").Contains (symbol) => PropertyValueConstraintRewriteStrategy.Instance,
+          _ => DefaultConstraintRewriteStrategy.Instance
+      };
+    }
+
+    private static bool IsNotAutomaticallyRewritable (
+        ExpressionSyntax node,
+        ISymbol symbol,
+        SemanticModel model,
+        INamedTypeSymbol rhinoMocksConstraintsIsSymbol,
+        INamedTypeSymbol rhinoMocksConstraintsPropertySymbol)
+    {
+      if (node.Parent is not BinaryExpressionSyntax parentBinaryExpression)
+      {
+        return false;
+      }
+
+      var nextExpressionSymbol = model.GetSymbolInfo (parentBinaryExpression.Right).Symbol;
+      if (node.IsEquivalentTo (parentBinaryExpression.Right, false))
+      {
+        nextExpressionSymbol = model.GetSymbolInfo (parentBinaryExpression.Left).Symbol;
+      }
+
+      return (rhinoMocksConstraintsPropertySymbol.GetMembers ("Value").Contains (symbol, SymbolEqualityComparer.Default)
+              || rhinoMocksConstraintsIsSymbol.GetMembers ("NotNull").Contains (symbol, SymbolEqualityComparer.Default))
+             && (rhinoMocksConstraintsPropertySymbol.GetMembers ("Value").Contains (nextExpressionSymbol, SymbolEqualityComparer.Default)
+                 || rhinoMocksConstraintsIsSymbol.GetMembers ("NotNull").Contains (nextExpressionSymbol, SymbolEqualityComparer.Default));
+    }
+  }
+}
